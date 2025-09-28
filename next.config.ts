e3b37ff16bd8a5019c1b7a911bfcff3b9bedc8fb
mirzomurotovshaxboz@gmail.com <shakhboz.m@lumex.agency>
2025-09-29 03:48:48 +0500

    netlify render

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export", // tells Next.js to generate static HTML
 };
 
 export default nextConfig;
